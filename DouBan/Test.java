9aaa3137d385b85f6669932f81672e0f2419a04a
KeplerWang <1692705146@qq.com>
2019-07-18 11:02:29 +0800

    添加jar包

--- DouBan/Test.java
+++ DouBan/Test.java
@@ -2,6 +2,10 @@ package douBan;
 
 public class Test {
     public static void main(String[] args)throws Exception{
-        new DouBanWebCrawler(249).getInformation();
+	    //使用须知：在DouBanWebCrawler.java中修改 库名 表名 用户名 密码
+	    //导入两个jar包 执行Test即可
+	    //支持指定爬的行数<=250 起始页
+	    //默认25条 从第一页开始爬
+    	    new DouBanWebCrawler(249).getInformation();
     }
 }
