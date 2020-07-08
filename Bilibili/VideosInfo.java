906436d7c3cc3b53634b8f8f4a95b972afe37178
KeplerWang <48934416+KeplerWang@users.noreply.github.com>
2020-07-08 15:32:27 +0800

    Update VideosInfo.java

--- Bilibili/VideosInfo.java
+++ Bilibili/VideosInfo.java
@@ -7,7 +7,7 @@ public class VideosInfo {
     String description;
     String videoLength;
     String picLocation;
-    int coinNum;//投笔
+    int coinNum;//投币
     int likeNum;//点赞
     int favoriteNum;//收藏
     int shareNum;//分享
